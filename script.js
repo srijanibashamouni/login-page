383baa7f7abdcbf0f0ea914a128067cd0c9c10fa
Srijani Bashamouni <srijanibashamouni265@gmail.com>
2026-03-17 11:58:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let message = document.getElementById("message");
+
+    // Simple validation (demo purpose)
+    if (username === "admin" && password === "1234") {
+        message.style.color = "green";
+        message.textContent = "Login Successful!";
+    } else {
+        message.style.color = "red";
+        message.textContent = "Invalid Username or Password!";
+    }
+});
